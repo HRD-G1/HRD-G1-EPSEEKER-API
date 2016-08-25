0653f89732f7e67d88dffa3a878ffe2e0ff9bf21
VisalGGEZ <visal012896@gmail.com>
2016-08-25 14:02:27 +0700

    add rest file

--- src/main/java/org/khmeracademy/epseeker/controllers/rest/RestUploadController.java
+++ src/main/java/org/khmeracademy/epseeker/controllers/rest/RestUploadController.java
@@ -17,70 +17,68 @@ import org.springframework.web.multipart.MultipartFile;
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.name.Rename;
 
-
-
 @RestController
 public class RestUploadController {
 
-	@RequestMapping(value="/rest/uploadphoto", method=RequestMethod.POST)
-	public Image uploadSingleFile(@RequestParam MultipartFile file){
+	@RequestMapping(value = "/rest/uploadphoto", method = RequestMethod.POST)
+	public Image uploadSinglePhoto(@RequestParam MultipartFile file) {
 
 		Image image = new Image();
 		System.out.println("FILE=" + file.getOriginalFilename());
-		
+
 		String filename = "";
 		Map<String, Object> map = new HashMap<String, Object>();
 		if (!file.isEmpty()) {
-			
+
 			String originalSavePath = "/opt/images/";
 			String thumbnailSavePath = "/opt/images/thumbnails/";
 			try {
 				filename = file.getOriginalFilename();
-				
+
 				byte[] bytes = file.getBytes();
-				
+
 				// UNIQUE UNIVERSAL ID
 				UUID uuid = UUID.randomUUID();
-				
+
 				String randomUUIDFileName = uuid.toString();
-				
+
 				String extension = filename.substring(filename.lastIndexOf(".") + 1);
-				
+
 				System.out.println(originalSavePath);
-				
+
 				File originalPath = new File(originalSavePath);
-				
+
 				if (!originalPath.exists()) {
 					System.out.println("NOT EXISTS");
 					originalPath.mkdirs();
 				}
-				
+
 				File thumbnailPath = new File(thumbnailSavePath);
-				
+
 				if (!thumbnailPath.exists()) {
 					System.out.println("NOT EXISTS");
 					thumbnailPath.mkdirs();
 				}
-				
+
 				filename = randomUUIDFileName + "." + extension;
-				
+
 				BufferedOutputStream stream = new BufferedOutputStream(
 						new FileOutputStream(new File(originalSavePath + filename)));
 				stream.write(bytes);
 				stream.close();
 				try {
-//					BufferedImage watermarkImage = ImageIO.read(new File("543a6de1-728e-4fe2-afa5-657084ff0001.jpg"));
-					//TODO: USING THUMBNAILS TO RESIZE THE IMAGE
-					Thumbnails.of(originalSavePath + filename)
-						.forceSize(300, 300)
-						.toFiles(thumbnailPath, Rename.NO_CHANGE);
+					// BufferedImage watermarkImage = ImageIO.read(new
+					// File("543a6de1-728e-4fe2-afa5-657084ff0001.jpg"));
+					// TODO: USING THUMBNAILS TO RESIZE THE IMAGE
+					Thumbnails.of(originalSavePath + filename).forceSize(300, 300).toFiles(thumbnailPath,
+							Rename.NO_CHANGE);
 				} catch (Exception ex) {
-					
-					stream = new BufferedOutputStream(new FileOutputStream(new File(thumbnailSavePath +  filename)));
+
+					stream = new BufferedOutputStream(new FileOutputStream(new File(thumbnailSavePath + filename)));
 					stream.write(bytes);
 					stream.close();
 				}
-				System.out.println("Present Project Directory : "+  originalSavePath + filename);
+				System.out.println("Present Project Directory : " + originalSavePath + filename);
 				image.setOriginalImage("http://localhost:3333/resources/" + filename);
 				image.setThumbnailImage("http://localhost:3333/resources/thumbnails/" + filename);
 				System.out.println("You successfully uploaded " + originalSavePath + filename + "!");
@@ -94,11 +92,64 @@ public class RestUploadController {
 			System.out.println("You failed to upload " + filename + " because the file was empty.");
 			return image;
 		}
-		
+
 	}
 	
-//	@RequestMapping(value="/rest/uploadtest", method=RequestMethod.POST)
-//	public String updateTest(@RequestParam("file") CommonsMultipartFile file){
-//		return file.getOriginalFilename();
-//	}
+	@RequestMapping(value = "/rest/uploadfile", method = RequestMethod.POST)
+	public Image uploadSingleFile(@RequestParam MultipartFile file) {
+
+		Image image = new Image();
+		System.out.println("FILE=" + file.getOriginalFilename());
+
+		String filename = "";
+		Map<String, Object> map = new HashMap<String, Object>();
+		if (!file.isEmpty()) {
+
+			String originalSavePath = "/opt/files/";
+			try {
+				filename = file.getOriginalFilename();
+
+				byte[] bytes = file.getBytes();
+
+				// UNIQUE UNIVERSAL ID
+				UUID uuid = UUID.randomUUID();
+
+				String randomUUIDFileName = uuid.toString();
+
+				String extension = filename.substring(filename.lastIndexOf(".") + 1);
+
+				System.out.println(originalSavePath);
+
+				File originalPath = new File(originalSavePath);
+
+				if (!originalPath.exists()) {
+					System.out.println("NOT EXISTS");
+					originalPath.mkdirs();
+				}
+
+
+
+				filename = randomUUIDFileName + "." + extension;
+
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File(originalSavePath + filename)));
+				stream.write(bytes);
+				stream.close();
+			
+				System.out.println("Present Project Directory : " + originalSavePath + filename);
+				image.setOriginalImage("http://localhost:3333/files/" + filename);
+				System.out.println("You successfully uploaded " + originalSavePath + filename + "!");
+				return image;
+			} catch (Exception e) {
+				System.out.println("You failed to upload " + filename + " => " + e.getMessage());
+				map.put("FILE_URL", originalSavePath + File.separator + filename);
+				return image;
+			}
+		} else {
+			System.out.println("You failed to upload " + filename + " because the file was empty.");
+			return image;
+		}
+
+	}
+
 }
